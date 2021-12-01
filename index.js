bec404071e8f950f7b7cc3f4e072d7a45c8e4b91
Diegudeveloper <diegudeveloper@gmail.com>
2021-12-01 10:42:13 +0100

    colocacion de la descripcion del avocado con javascript en ela ventana modal

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-const baseUrl = "https://platzi-avo.vercel.app";
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = 'https://platzi-avo.vercel.app';
+const url = 'https://platzi-avo.vercel.app/api/avo';
 
 const containerInfo = document.querySelector('.container_main');
 
-window.fetch(`${baseUrl}/api/avo`)
-.then((respuesta) => respuesta.json())
-.then((respuestaJson) => {
+function showDescription(name) {
+  const domElement = document.querySelector('.contenido');
+  console.log(domElement);
+  domElement.innerHTML =  `<p>${name}<p>`;
+  console.log(domElement);
 
+  const namemodal = document.querySelector('.name_modal');
+  namemodal.innerHTML = `<p>${element.name}<p>`
+}
+
+window
+  .fetch(`${baseUrl}/api/avo`)
+  .then((respuesta) => respuesta.json())
+  .then((respuestaJson) => {
     respuestaJson.data.forEach((data) => {
-
-        containerInfo.innerHTML += `
-        <div class="container_avocado">
+      containerInfo.innerHTML += `
+        <div class="container_avocado" >
             <div class="container_imgAvocado">
                 <img src = ${baseUrl}${data.image} alt="">
             </div>
                 <div class="container_text_avocado">
                 <h2 class="nameAvocado">${data.name}</h2>
                 <h3 class="tasteAvocado">${data.attributes.taste}</h3>
-                <h3 class="priceAvocado"> € ${data.price}</h3>               
-                <label for="btn-modal" class="lbl-modal btn_description">Description...</label>
+                <h3 class="priceAvocado"> € ${data.price}</h3>
+                <label onclick="showDescription('${data.attributes.description} ${data.name}') " for="btn-modal" class="lbl-modal- btn_description name_modal">Description...</label>
             </div>
-        </div> 
-        `    
+        </div>
+        `;
     })
-
-});
+}); 
