3d3474fdd3e8c77b19b176d0ca150514e12793c2
Diegudeveloper <diegudeveloper@gmail.com>
2022-01-22 21:06:33 +0100

    configuracion y mejora del section Menu

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+//Header//
+
+const searchBtn = document.querySelector(".fa-search");
+const searchContainer = document.querySelector(".search");
+const btn_menu = document.querySelector(".btn_menu");
+const navbar = document.querySelector(".navbar");
+
+const btnUser = document.querySelector(".fa-user");
+
+const btnForm = document.querySelector(".container_login");
+const btnX = document.querySelector("#btn_close");
+
+btnUser.addEventListener('click', () => {
+    btnForm.classList.add('active');
+});
+
+btnX.addEventListener('click', () => {
+    btnForm.classList.remove('active');
+});
+
+
+btn_menu.addEventListener("click", () => {
+    navbar.classList.toggle('navbar_active');
+});
+
+searchBtn.addEventListener("click", () => {
+    searchBtn.classList.toggle('fa-times');
+    searchContainer.classList.toggle('active');
+});
+
+
+
+
+
+
 const baseUrl = 'https://platzi-avo.vercel.app';
 const url = 'https://platzi-avo.vercel.app/api/avo';
 
